dac6b8d3d2a90b112501674de74a485dd79e71e4
tequila01-svg <onifadeayobami008@gmail.com>
2024-11-05 17:59:29 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,9 @@
+class Employees:
+    def __init__(self):
+        print("employee created")
+        
+        
+    def __init__(self):
+        print("destructor called, employee deleted!!")
+obj=Employees()
+del obj
